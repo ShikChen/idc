7538256e35b94c260b5c6626dd3cf43d31f795fa
Shik Chen <shik.chen@gmail.com>
2026-01-10 21:54:43 +0800

    feat: create idc-server template with Xcode

---
+++ idc-server/idc-server/idc_serverApp.swift
@@ -0,0 +1,17 @@
+//
+//  idc_serverApp.swift
+//  idc-server
+//
+//  Created by Shik Chen on 2026/1/10.
+//
+
+import SwiftUI
+
+@main
+struct idc_serverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ idc-server/idc-serverTests/idc_serverTests.swift
@@ -0,0 +1,16 @@
+//
+//  idc_serverTests.swift
+//  idc-serverTests
+//
+//  Created by Shik Chen on 2026/1/10.
+//
+
+import Testing
+
+struct idc_serverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
